99cb7c0b4ce06434818972bf1cc5c7102e58a157
rumen_stankov <rumen_stankov@8e0bbdfe-a38e-451f-afe2-c54cb91658cd>
2009-08-26 05:16:44 +0000

    git-svn-id: https://subversion.assembla.com/svn/JQueryGrid/SmartWebParts.Web.UI/trunk@3 8e0bbdfe-a38e-451f-afe2-c54cb91658cd

---
+++ JQueryGrid/JsonResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trirand.Web.UI.WebControls
+{
+    internal class JsonResponse
+    {
+        public int page { get; set; }
+        public int total { get; set; }
+        public int records { get; set; }
+        public JsonRow[] rows { get; set; }
+    }
+}
---
+++ JQueryGrid/JsonRow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trirand.Web.UI.WebControls
+{
+    internal class JsonRow
+    {
+        public string id;
+        public object[] cell { get; set; }
+    }
+}
